b73e57925e4c3ebd44d260836e1c7ba8331ef06c
Cililocwee <corrie.stroup@gmail.com>
2022-11-24 10:52:12 +0700

    Updates

--- src/randomUniqueNum.js
+++
@@ -1,9 +0,0 @@
-const randomUniqueNum = (range, count) => {
-  let nums = new Set();
-  while (nums.size < count) {
-    nums.add(Math.floor(Math.random() * (range - 1 + 1) + 1));
-  }
-  return [...nums];
-};
-
-export default randomUniqueNum;
